7642e00be826222a3f374531119964f527cbd2ae
eless_000 <eless_000@BODA>
2014-05-10 02:39:36 +0200

    BTreeFile (cons, close, destroy)

--- B Tree/btree/btree/BTreeFile.java
+++ B Tree/btree/btree/BTreeFile.java
@@ -1,35 +1,167 @@
 package btree;
 
+import java.io.IOException;
+
+import bufmgr.HashEntryNotFoundException;
+import bufmgr.InvalidFrameNumberException;
+import bufmgr.PageUnpinnedException;
+import bufmgr.ReplacerException;
+import global.GlobalConst;
+import global.PageId;
 import global.RID;
+import global.SystemDefs;
+import heap.HFBufMgrException;
+import heap.HFDiskMgrException;
+import diskmgr.Page;
 
-public class BTreeFile {
+public class BTreeFile extends IndexFile implements GlobalConst {
+	private BTreeHeaderPage headerPage;	
+	private PageId  headerPageId;
+	private String  dbname; 
+	
+	public BTreeFile(String filename) throws HFDiskMgrException{
+		headerPageId = get_file_entry(filename);
+	    headerPage= new  BTreeHeaderPage( headerPageId);       
+	    dbname = new String(filename);
+	}
+	
+	 public BTreeFile(String filename,int keytype,int keysize,int delete_fashion) throws HFDiskMgrException{
 
-	public BTreeFile(String string, int keyType, int i, int j) {
-		// TODO Auto-generated constructor stub
+		 headerPageId = get_file_entry(filename);
+		 if( headerPageId==null){ //file not exist
+			 headerPage = new  BTreeHeaderPage(); 
+			 headerPageId = headerPage.getPageId();
+			 add_file_entry(filename, headerPageId);
+//			 headerPage.set_magic0(MAGIC0);
+			 headerPage.set_rootId(new PageId(INVALID_PAGE));
+			 headerPage.set_keyType((short)keytype);    
+			 headerPage.set_maxKeySize(keysize);
+			 headerPage.set_deleteFashion( 0 ); ///<<<<<<<<<
+//			 headerPage.set_keyType(NodeType.BTHEAD); ///<<<<<<<<
+		 }else {
+			 headerPage = new BTreeHeaderPage( headerPageId );  
+		 } 
+	     dbname=new String(filename);
+	}
+	 
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+	 
+	 
+	 public void close() throws ReplacerException, PageUnpinnedException, HashEntryNotFoundException, InvalidFrameNumberException{
+		 if ( headerPage!=null) { //not deleted
+			 SystemDefs.JavabaseBM.unpinPage(headerPageId, true);
+			 headerPage=null;
+		 }  
+	 }
+	 
+	 public void destroyFile() throws HFBufMgrException, HFDiskMgrException, ConstructPageException, IOException, IteratorException{
+		 if( headerPage != null) {
+			 PageId pid= headerPage.get_rootId();
+			 if( pid.pid != INVALID_PAGE) destroyFileRecursive(pid);
+			 unpinPage(headerPageId,false);
+			 freePage(headerPageId);      
+			 delete_file_entry(dbname);
+			 headerPage=null;
+		 }
+	 }
+	 
+	 private void  destroyFileRecursive(PageId pageId) throws HFBufMgrException, ConstructPageException, IOException, IteratorException{
+		 Page page = new Page(); 
+		 pinPage(pageId,page,false);
+		 
+		 int keyType = headerPage.get_keyType();
+		 BTSortedPage sortedPage = new BTSortedPage(page, keyType); //pageID<<
+			      
+		 if (sortedPage.getType() == NodeType.INDEX) {
+			 BTIndexPage indexPage = new BTIndexPage(page, keyType); //pageID<<
+			 
+			 RID rid = new RID();
+			 PageId childId; 
+			 KeyDataEntry entry;
+			 for (entry = indexPage.getFirst(rid) ; entry!=null ; entry = indexPage.getNext(rid)){ 
+				 childId = ((IndexData)(entry.data)).getData();
+				 destroyFileRecursive(childId);
+			 }
+			 
+		 }else { //leafPage 
+			 unpinPage(pageId,false);
+			 freePage(pageId);
+		 }      
 	}
 
-	public BTreeFile(String string) {
-		// TODO Auto-generated constructor stub
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+
+	
+	 private void pinPage(PageId pageno, Page page, boolean emptyPage)throws HFBufMgrException {
+		try {
+			SystemDefs.JavabaseBM.pinPage(pageno, page, emptyPage);
+		} catch (Exception e) {
+			throw new HFBufMgrException(e, "Heapfile.java: pinPage() failed");
+		}
+	 } 
+		
+	 private void unpinPage(PageId pageno, boolean dirty)
+				throws HFBufMgrException {
+		try {
+			SystemDefs.JavabaseBM.unpinPage(pageno, dirty);
+		} catch (Exception e) {
+			throw new HFBufMgrException(e, "Heapfile.java: unpinPage() failed");
+		}
 	}
 
-	public BTreeHeaderPage getHeaderPage() {
+	private void freePage(PageId pageno) throws HFBufMgrException {
+		try {
+			SystemDefs.JavabaseBM.freePage(pageno);
+		} catch (Exception e) {
+			throw new HFBufMgrException(e, "Heapfile.java: freePage() failed");
+		}
+	}
+
+	private PageId newPage(Page page, int num) throws HFBufMgrException {
+		PageId tmpId = new PageId();
+		try {
+			tmpId = SystemDefs.JavabaseBM.newPage(page, num);
+		} catch (Exception e) {
+			throw new HFBufMgrException(e, "Heapfile.java: newPage() failed");
+		}
+		return tmpId;
+	}
+
+	private PageId get_file_entry(String filename) throws HFDiskMgrException {
+		PageId tmpId = new PageId();
+		try {
+			tmpId = SystemDefs.JavabaseDB.get_file_entry(filename);
+		} catch (Exception e) {
+			throw new HFDiskMgrException(e,"Heapfile.java: get_file_entry() failed");
+		}
+		return tmpId;
+	}
+
+	private void add_file_entry(String filename, PageId pageno)throws HFDiskMgrException {
+		try {
+			SystemDefs.JavabaseDB.add_file_entry(filename, pageno);
+		} catch (Exception e) {
+			throw new HFDiskMgrException(e,"Heapfile.java: add_file_entry() failed");
+		}
+	}
+
+	private void delete_file_entry(String filename) throws HFDiskMgrException {
+		try {
+			SystemDefs.JavabaseDB.delete_file_entry(filename);
+		} catch (Exception e) {
+			throw new HFDiskMgrException(e,"Heapfile.java: delete_file_entry() failed");
+		}
+	}
+
+
+	public BTLeafPage findRunStart(KeyClass key, RID curRid) {
 		// TODO Auto-generated method stub
 		return null;
-	}
-
-	public void insert(KeyClass key, RID rid) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void close() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void Delete(IntegerKey integerKey, RID rid) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	public BTFileScan new_scan(KeyClass lowkey, KeyClass hikey) {
@@ -37,24 +169,23 @@ public class BTreeFile {
 		return null;
 	}
 
-	public void destroyFile() {
+	public void traceFilename(String string) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	public void traceFilename(String string) {
+	public void insert(KeyClass key, RID rid) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public BTreeHeaderPage getHeaderPage() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	public boolean Delete(KeyClass key, RID rid) {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
-	public BTLeafPage findRunStart(KeyClass key, RID curRid) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
--- B Tree/btree/btree/BTreeHeaderPage.java
+++ B Tree/btree/btree/BTreeHeaderPage.java
@@ -5,6 +5,14 @@ import global.RID;
 
 public class BTreeHeaderPage {
 
+	public BTreeHeaderPage(PageId headerPageId) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public BTreeHeaderPage() {
+		// TODO Auto-generated constructor stub
+	}
+
 	public PageId get_rootId() {
 		// TODO Auto-generated method stub
 		return null;
@@ -13,6 +21,31 @@ public class BTreeHeaderPage {
 	public int get_keyType() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	public void set_rootId(PageId pageId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public PageId getPageId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void set_keyType(short keytype) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void set_maxKeySize(int keysize) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void set_deleteFashion(int delete_fashion) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
